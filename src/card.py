6df10080e68870f49080d9d0f819a5422f7e0c69
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2021-10-12 18:07:37 +0200

    [Card] Card class - Create a card - Rotation - toString for debug - get a particular value

---
+++ src/card.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Card:
+    """
+    A class used to represent a card
+
+
+    Attributes
+    ----------
+    sides : List[int]
+        a list representing the witch color / part
+    rotation_count: int
+        a int representing the number of 90° rotation made by the piece
+
+    Methods
+    -------
+    tostring()
+        return sides in a string separed by ", "
+    rotation()
+        rotate the card by 90° and increment the rotation_count by one
+    getLeft() - getUp() - getRight() - getDown()
+        return the corresponding value for the corresponding side
+    """
+
+    def __init__(self, left, up, right, down) -> None:
+        self.sides: List[int] = [left, up, right, down]
+        self.rotation_count: int = 0
+
+    def toString(self) -> str:
+        return ", ".join(map(str, self.sides))
+
+    def rotation(self) -> None:
+        self.rotation_count = self.rotation_count + 1
+        self.sides.insert(0, self.sides.pop(-1))
+
+    def getLeft(self) -> int:
+        return self.sides[0]
+
+    def getUp(self) -> int:
+        return self.sides[1]
+
+    def getRight(self) -> int:
+        return self.sides[2]
+
+    def getDown(self) -> int:
+        return self.sides[3]
